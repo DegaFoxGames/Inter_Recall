c5635a1635509a4f2d8abaca3fbbeb900f0cd85f
Reinaldo <reinaldodipietro@gmail.com>
2018-11-14 13:07:09 -0200

    Inimigo (perseguir e atirar (sem o tiro ainda))

--- Recall/Assets/Scripts/Agente.cs
+++ Recall/Assets/Scripts/Agente.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Agente : MonoBehaviour {
+public class Agente : MonoBehaviour
+{
 
     private bool eLadoDireito;
 
@@ -11,7 +12,7 @@ public class Agente : MonoBehaviour {
     private Animator anim;
 
     private float tempoIdle;
-    [SerializeField] 
+    [SerializeField]
     private float duracaoIdle;
 
     private float tempoPatrulhar;
@@ -28,39 +29,42 @@ public class Agente : MonoBehaviour {
     public float playerDistancia;
     public float ataqueDistancia;
     public GameObject player;
-     
+
 
     // Use this for initialization
-    void Start () {
-       // direcao = Direcao.DIREITA;
+    void Start()
+    {
+        // direcao = Direcao.DIREITA;
         anim = GetComponent<Animator>();
         eLadoDireito = true;
         estaPatrulhando = false;
         atacar = false;
 
-        vel = 1;
-        ataqueDistancia = 6;
-        duracaoIdle = 5;
+        vel = 3;
+        ataqueDistancia = 10;
+        duracaoIdle = 2;
         duracaoPatrulhar = 5;
-        duracaoAtacar = 5;
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        duracaoAtacar = 2.4f;
+    }
+
+    // Update is called once per frame
+    void Update()
     {
 
         MudarEstado();
 
         playerDistancia = transform.position.x - player.transform.position.x;
 
-
-
         if (Mathf.Abs(playerDistancia) < ataqueDistancia)
         {
             atacar = true;
             estaPatrulhando = false;
-            Idle();
-            Atirar();
+
+            if (atacar && !estaPatrulhando)
+            {
+                Idle();
+                Atirar();
+            }
         }
         else
         {
@@ -77,13 +81,13 @@ public class Agente : MonoBehaviour {
 
     Vector2 PegarDirecao()
     {
-         return eLadoDireito ? Vector2.right : Vector2.left;
+        return eLadoDireito ? Vector2.right : Vector2.left;
     }
 
     void MudarDirecao()
     {
         eLadoDireito = !eLadoDireito;
-        transform.localScale = new Vector3(-transform.localScale.x, 1, 1);   
+        transform.localScale = new Vector3(-transform.localScale.x, 1, 1);
     }
 
 
@@ -137,7 +141,7 @@ public class Agente : MonoBehaviour {
             }
         }
     }
-    
+
 
     void MudarEstado()
     {
