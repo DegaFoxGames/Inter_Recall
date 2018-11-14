fe9106cce0b45a8e01da2bd8a2125e2a35b4370a
Reinaldo <reinaldodipietro@gmail.com>
2018-11-14 00:25:50 -0200

    Upload All
    Upando tudo de novo, na conta da FOXGAMES

---
+++ Recall/Assets/Scripts/Agente.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agente : MonoBehaviour {
+
+    private bool eLadoDireito;
+
+    [SerializeField]
+    private float vel;
+    private Animator anim;
+
+    private float tempoIdle;
+    [SerializeField] 
+    private float duracaoIdle;
+
+    private float tempoPatrulhar;
+    [SerializeField]
+    private float duracaoPatrulhar;
+
+    private float tempoAtacar;
+    [SerializeField]
+    private float duracaoAtacar;
+
+    public bool estaPatrulhando;
+    public bool atacar;
+
+    public float playerDistancia;
+    public float ataqueDistancia;
+    public GameObject player;
+     
+
+    // Use this for initialization
+    void Start () {
+       // direcao = Direcao.DIREITA;
+        anim = GetComponent<Animator>();
+        eLadoDireito = true;
+        estaPatrulhando = false;
+        atacar = false;
+
+        vel = 1;
+        ataqueDistancia = 6;
+        duracaoIdle = 5;
+        duracaoPatrulhar = 5;
+        duracaoAtacar = 5;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        MudarEstado();
+
+        playerDistancia = transform.position.x - player.transform.position.x;
+
+
+
+        if (Mathf.Abs(playerDistancia) < ataqueDistancia)
+        {
+            atacar = true;
+            estaPatrulhando = false;
+            Idle();
+            Atirar();
+        }
+        else
+        {
+            MudarEstado();
+        }
+    }
+
+    // métodos para movimento 
+
+    void Mover()
+    {
+        transform.Translate(PegarDirecao() * (vel * Time.deltaTime));
+    }
+
+    Vector2 PegarDirecao()
+    {
+         return eLadoDireito ? Vector2.right : Vector2.left;
+    }
+
+    void MudarDirecao()
+    {
+        eLadoDireito = !eLadoDireito;
+        transform.localScale = new Vector3(-transform.localScale.x, 1, 1);   
+    }
+
+
+
+    // métodos para animação
+
+    void Idle()
+    {
+        tempoIdle += Time.deltaTime;
+        if (tempoIdle <= duracaoIdle)
+        {
+            anim.SetBool("estaPatrulhando", estaPatrulhando);
+            tempoPatrulhar = 0;
+        }
+        else
+        {
+            estaPatrulhando = true;
+        }
+    }
+
+    void Patrulhar()
+    {
+        tempoPatrulhar += Time.deltaTime;
+        if (tempoPatrulhar <= duracaoPatrulhar)
+        {
+            anim.SetBool("estaPatrulhando", estaPatrulhando);
+            Mover();
+            tempoIdle = 0;
+        }
+        else
+        {
+            estaPatrulhando = false;
+        }
+    }
+
+    void Atirar()
+    {
+        if (playerDistancia < 0 && !eLadoDireito || playerDistancia > 0 && eLadoDireito)
+        {
+            MudarDirecao();
+        }
+
+        if (atacar)
+        {
+            tempoAtacar += Time.deltaTime;
+
+            if (tempoAtacar >= duracaoAtacar)
+            {
+                anim.SetTrigger("estaAtirando");
+                tempoAtacar = 0;
+            }
+        }
+    }
+    
+
+    void MudarEstado()
+    {
+        if (!atacar)
+        {
+            if (!estaPatrulhando)
+            {
+                Idle();
+            }
+
+            else
+            {
+                Patrulhar();
+            }
+        }
+    }
+
+    public void ResetarAtacar()
+    {
+        atacar = false;
+    }
+}
---
+++ Recall/Assets/Scripts/Jogador.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour {
+
+    Rigidbody2D rb;
+    public int velocidadeJogador;
+    public int velocidadeJogadorRun;
+    public int forcaPulo;
+    float duracaoPulo;
+    Animator animator;
+    public GameObject player; // gameobject para inserir o player e mudar a escala dele
+    public GameObject arm; // gameobject para inserir o braço e mudar a escala dele
+
+
+    enum Estados {PARADO, ANDANDO, CORRENDO, PULANDO}
+    Estados estado;
+
+    public enum Lado { DIREITA, ESQUERDA }
+    public Lado lado;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        velocidadeJogador = 5;
+        velocidadeJogadorRun = 10;
+        forcaPulo = 15;
+        estado = Estados.ANDANDO;
+        lado = Lado.DIREITA;
+    }
+    
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if (estado == Estados.ANDANDO)
+        {
+            Andando();
+            
+            duracaoPulo = 0;
+            
+        }
+
+        else if (estado == Estados.PULANDO)
+        {
+            Pulando();
+            duracaoPulo -= Time.deltaTime;
+        }
+
+        // print(duracaoPulo);
+        // print(estado);
+    }
+
+    void Andando()
+    {
+        Vector2 vel = rb.velocity;
+        vel.x = Input.GetAxis("Horizontal") * velocidadeJogador;
+
+        if (vel.x > 0)
+        {
+            lado = Lado.DIREITA;     
+        }
+
+        if (vel.x < 0)
+        {
+            lado = Lado.ESQUERDA;     
+        }
+
+
+        if (lado == Lado.ESQUERDA)
+        {
+            //GetComponent<SpriteRenderer>().flipX = true;
+
+
+             Vector3 newScalep = player.transform.localScale;
+             newScalep.x = -1;
+             player.transform.localScale = newScalep;
+
+             Vector3 newScalea = arm.transform.localScale;
+             newScalea.x = -1;
+             newScalea.y = -1;
+
+             arm.transform.localScale = newScalea;
+        }
+
+        else if (lado == Lado.DIREITA)
+        {
+            //GetComponent<SpriteRenderer>().flipX = false;
+
+            Vector3 newScalep = player.transform.localScale;
+            newScalep.x = 1;
+            player.transform.localScale = newScalep;
+
+            Vector3 newScalea = arm.transform.localScale;
+            newScalea.x = 1;
+            newScalea.y = 1;
+
+            arm.transform.localScale = newScalea;
+        }
+        
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            vel.y = forcaPulo;
+            duracaoPulo += 0.5f;  
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            vel.x = Input.GetAxis("Horizontal") * velocidadeJogadorRun;
+        }
+
+      
+        rb.velocity = vel;
+        animator.SetInteger("Andando", (int)vel.x);
+
+    }
+
+    void Pulando()
+    {
+        if (duracaoPulo <= 0)
+        {
+            Vector2 vel = rb.velocity;
+            vel.x = Input.GetAxis("Horizontal") * velocidadeJogador;
+            rb.velocity = vel;
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //print("Enter");
+        if (collision.tag == "Plataforma_tag")
+        {
+
+        }
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //print("Stay");
+        if (collision.tag == "Plataforma_tag")
+        {
+            if (estado == Estados.PULANDO)
+            {
+                estado = Estados.ANDANDO;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //print("Exit");
+        if (collision.tag == "Plataforma_tag")
+        {
+            if (estado == Estados.ANDANDO)
+            {
+                estado = Estados.PULANDO;
+            }
+        }
+    }
+}
